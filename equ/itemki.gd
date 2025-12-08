4e1303896a9a788b08cddeb92ce676da0b7c636b
milka36 <emilka.turon.pl@interia.pl>
2025-12-08 19:54:14 +0100

    inventory
    zaczęte inventory z tutoriala jeszcze niezbyt sprawne

--- equ/itemki.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-func _ready():
-	if randi() % 2 == 0:
-		$TextureRect.texture = load("res://sprites/do_zdobycia/sus.png")
-	else:
-		$TextureRect.texture = load("res://sprites/do_zdobycia/lopata.png")
---
+++ itemy/script/Inventory.gd
@@ -0,0 +1,6 @@
+class_name Inventory extends Resource
+
+@export var slots: Array[Slot]
+
+func add_item() -> void:
+	pass
