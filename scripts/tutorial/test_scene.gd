f60826e1965c2f53025b0e36ccf29dc5894b8170
Buzabs <bonarzofia@gmail.com>
2026-02-03 18:55:39 +0100

    Cursor test
    Added changing cursors on hover

--- scripts/tutorial/test_scene.gd
+++ scripts/tutorial/test_scene.gd
@@ -2,7 +2,9 @@ extends Node
 
 @export var pause_scene: PackedScene
 
-
+var movement_cursor = load("res://sprites/Other/movement_cursor.png")
+var interaction_cursor = load("res://sprites/Other/cog_cursor.png")
+var door_cursor = load("res://sprites/Other/door_curor.png")
 
 func _process(_delta):
 	
@@ -14,4 +16,43 @@ func _process(_delta):
 		Pausa.toggle_pause(pause_scene)
 
 	
-	
+
+func _on_computer_click_mouse_entered() -> void:
+	Input.set_custom_mouse_cursor(interaction_cursor)
+
+
+func _on_computer_click_mouse_exited() -> void:
+	Input.set_custom_mouse_cursor(movement_cursor)
+
+
+func _on_drzwi_area_mouse_entered() -> void:
+	Input.set_custom_mouse_cursor(door_cursor)
+
+
+
+func _on_drzwi_area_mouse_exited() -> void:
+	Input.set_custom_mouse_cursor(movement_cursor)
+
+
+func _on_area_2d_3_mouse_entered() -> void:
+	Input.set_custom_mouse_cursor(interaction_cursor)
+
+
+func _on_area_2d_3_mouse_exited() -> void:
+	Input.set_custom_mouse_cursor(movement_cursor)
+
+
+func _on_area_2d_2_mouse_entered() -> void:
+	Input.set_custom_mouse_cursor(interaction_cursor)
+
+
+func _on_area_2d_2_mouse_exited() -> void:
+	Input.set_custom_mouse_cursor(movement_cursor)
+
+
+func _on_drzwi_mag_mouse_entered() -> void:
+	Input.set_custom_mouse_cursor(door_cursor)
+
+
+func _on_drzwi_mag_mouse_exited() -> void:
+	Input.set_custom_mouse_cursor(movement_cursor)
